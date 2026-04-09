5f83bc0f552df0294b9777217f9975245dc85503
xjose999 <josejoestar80@gmail.com>
2026-04-08 20:48:42 -0500

    Ejercicio 4

---
+++ Downloads/Talleres-0/CuentaBancaria.java
@@ -0,0 +1,14 @@
+public class CuentaBancaria {
+    int numero;
+    int saldo;
+    String titular;
+
+
+public void mostrarCuenta() {
+    System.out.println("Su número de cuenta es " + numero);
+    System.out.println("Su saldo en la cuenta es de " + saldo);
+    System.out.println("El titular de la cuenta es " + titular);
+
+}
+
+}
