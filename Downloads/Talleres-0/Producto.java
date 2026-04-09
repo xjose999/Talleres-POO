08b7377ce6da8865814e69bdb678b1e7e4583279
xjose999 <josejoestar80@gmail.com>
2026-04-08 21:37:08 -0500

    Ejercicio 8

--- Downloads/Talleres-0/Producto.java
+++ Downloads/Talleres-0/Producto.java
@@ -9,4 +9,24 @@ public class Producto {
         System.out.println("Su producto tiene un valor de " + precio);
     }
 
+    public static void main (String[] args) {
+
+        Producto product1 = new Producto();
+        product1.nombre = "Juguete Max Steel";
+        product1.stock = 60;
+        product1.precio = 200;
+
+        Producto product2 = new Producto();
+        product2.nombre = "Juguete Barbie";
+        product2.stock = 25;
+        product2.precio = 250;
+
+        Producto product3 = new Producto();
+        product3.nombre = "Juguete HotWheels";
+        product3.stock  = 320;
+        product3.precio = 5;
+
+        product1.mostrarProducto();
+        product2.mostrarProducto();
+    }
 }
