7b44eb09aab3c727e89ee4158e082afc84ef5e72
xjose999 <josejoestar80@gmail.com>
2026-04-08 20:20:35 -0500

    Ejercicio 5

---
+++ Downloads/Talleres-0/Mascota.java
@@ -0,0 +1,12 @@
+public class Mascota {
+    String nombre;
+    String especie;
+    int edad;
+
+public void mostrarMascota() {
+    System.out.println("Su mascota se llama " + nombre);
+    System.out.println("Su mascota es de raza " + especie);
+    System.out.println("Su mascota tiene " + edad);
+}
+
+}
