ad453b2080b9a9d8775dc34d3e4f9c324a0de0ab
xjose999 <josejoestar80@gmail.com>
2026-04-08 21:04:58 -0500

    Ejercicio 6

--- Downloads/Talleres-0/Estudiante.java
+++ Downloads/Talleres-0/Estudiante.java
@@ -3,11 +3,26 @@ public class Estudiante {
     String codigo;
     int semestre;
 
+
     public void mostrarInfo() {
         System.out.println("Nombre: " + nombre);
         System.out.println("Código: " + codigo);
         System.out.println("Semestre: " + semestre + "°");
 
     }
+
+    public static void main (String[] args) {
+
+        Estudiante est1 = new Estudiante();
+        est1.nombre = "José Daniel Ballesteros";
+        est1.codigo = "7502520040";
+        est1.semestre = 2;
+
+        est1.mostrarInfo();
+
+    }
+
+
+
 
 }
